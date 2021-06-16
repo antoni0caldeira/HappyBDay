2e39ba709edf95f5ccb8a371f2eea64e64279a31
Bernardo Sousa <Bernas_17_cs@hotmail.com>
2021-06-16 11:15:02 +0100

    Addind the email functionality

---
+++ HappyBDay/Models/EmailModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace HappyBDay.Models
+{
+    public class EmailModel
+    {
+        [Required, Display(Name = "Email de destino"), EmailAddress]
+        public string Destino { get; set; }
+
+        [Required, Display(Name = "Assunto")]
+        public string Assunto { get; set; }
+
+        [Required, Display(Name = "Mensagem")]
+        public string Mensagem { get; set; }
+    }
+}
