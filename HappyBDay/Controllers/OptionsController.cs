4334ca11e986f7da65a84e504d5767b54768412a
Your Name <you@example.com>
2021-06-24 11:11:43 +0100

    changes in the view home (Login buttton)

--- HappyBDay/Controllers/OptionsController.cs
+++ HappyBDay/Controllers/OptionsController.cs
@@ -1,11 +1,13 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace HappyBDay.Controllers
-{
+{    
+    [Authorize]
     public class OptionsController : Controller
     {
         public IActionResult Index()
